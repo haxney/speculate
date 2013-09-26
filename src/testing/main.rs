474cdeebfa6823e72d3c9b0fa5e90aa21cd4ff3e
Daniel Hackney <dhackney@cs.brown.edu>
2013-09-26 18:58:01 -0400

    compare lex and parse times for samples
    There seems to be some kind of bug in servo-style, since it barfs on
    some CSS files.

--- src/testing/main.rs
+++ src/testing/main.rs
@@ -1,7 +1,9 @@
 extern mod cssparser;
 extern mod extra;
+extern mod servo_style;
 
 use std::io::file_reader;
+use std::task;
 use cssparser::*;
 use extra::time::precise_time_ns;
 
@@ -23,16 +25,46 @@ fn bench(inner: &fn()) -> u64 {
     return ns_end - ns_start;
 }
 
-fn main() {
-    let file = "sample-data/amazon11.css";
-    let reader = file_reader(&Path(file)).unwrap();
-
-    let b_time = do bench {
-        let mut a = parse_stylesheet_rules(tokenize(reader.read_c_str()));
-        for res in a {
-            print_res(res);
+fn bench_lex_one_file(path: &Path) -> Result<u64, ()> {
+    let p = path.clone();
+    do task::try {
+        let my_path = &p.clone();
+        let reader = file_reader(my_path).unwrap();
+        do bench {
+            let mut a = parse_stylesheet_rules(tokenize(reader.read_c_str()));
+            for res in a {
+                print_res(res);
+            }
         }
-    };
+    }
+}
 
-    println(format!("time taken: {:u}μs", b_time / 1000));
+fn bench_parse_one_file(path: &Path) -> Result<u64, ()> {
+    // This is really redundant
+    let p = path.clone();
+    do task::try {
+        let my_path = &p.clone();
+        let reader = file_reader(my_path).unwrap();
+        do bench {
+            servo_style::stylesheets::parse_stylesheet(reader.read_c_str());
+        }
+    }
+}
+
+fn main() {
+    let base_dir = &Path("sample-data");
+    let files = std::os::list_dir_path(base_dir);
+
+    for file in files.iter() {
+        let lex_time = bench_lex_one_file(file);
+        let parse_time = bench_parse_one_file(file);
+        match (lex_time, parse_time) {
+            (Ok(l), Ok(p)) =>
+                println(format!("{}:\tlex: {:.4f} ms\tparse: {:.4f} ms",
+                                file.filename().unwrap(),
+                                l as float / 1_000_000f,
+                                (p - l) as float / 1_000_000f)),
+            _ => println(format!("{}: ERROR", file.filename().unwrap())),
+        }
+    }
 }
