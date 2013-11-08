9548de2ebed1e39075790deadc83ca469033a500
Daniel Hackney <dhackney@cs.brown.edu>
2013-11-08 17:46:35 -0500

    Move `specfold` lexing to "spec_css/lib.rs"

--- src/spec_css/lib.rs
+++ src/spec_css/lib.rs
@@ -4,7 +4,36 @@ extern mod extra;
 extern mod css_lex;
 extern mod speculate;
 
+use css_lex::*;
+use speculate::*;
+use std::{num, task, vec};
+use std::comm::{SharedPort, SharedChan, stream};
+use extra::arc::Arc;
+
 static LOOKBACK: uint = 10;
+
+/**
+ * Collects the loop body results into a 2d vector.
+ *
+ * Each loop body sends its index and an optional result type. If the message
+ * received over the `port` is `None`, then stop listening and push the result
+ * over `chan`. If the message received is `Some(i, None)`, then clear the
+ * `i`-th result vector. If the message is `Some(i, Some(t))`, then add `t` to
+ * the `i`-th result vector.
+ */
+fn spawn_result_collector<T: Send + Clone>(port: SharedPort<Option<(int, Option<T>)>>, chan: Chan<~[T]>, size: uint) {
+    do task::spawn {
+        let mut results = vec::from_elem::<~[T]>(size, Default::default());
+        loop {
+            match port.recv() {
+                Some((idx, Some(val))) => results[idx].push(val),
+                Some((idx, None)) => results[idx].clear(),
+                None => break
+            }
+        }
+        chan.send(results.flat_map(|v| v.clone()));
+    }
+}
 
 /**
  * Find the start of the next token at or after `start`.
@@ -12,11 +41,58 @@ static LOOKBACK: uint = 10;
  * Backs up `LOOKBACK` characters and begins lexing until reaching or passing
  * `start`.
  *
- * This is somewhat inefficient, since it makes a copy of `input`.
+ * Assumes `input` has already been preprocessed.
  */
-pub fn next_token_start(input: &str, start: uint) -> uint {
-    let mut tokenizer = css_lex::tokenize(input);
+pub fn next_token_start(input: Arc<~str>, start: uint) -> uint {
+    let mut tokenizer = Tokenizer::new(input);
     tokenizer.position = if start < LOOKBACK {0} else {start - LOOKBACK};
     while tokenizer.position < start && tokenizer.next().is_some() {}
     tokenizer.position
+}
+
+pub fn spec_tokenize(input: ~str, num_iters: uint) -> ~[Node] {
+    let input = preprocess(input);
+    let css_len = input.len();
+    let str_arc = Arc::new(input);
+    let iter_size: uint = css_len / num_iters;
+    let (port, chan): (Port<Option<(int, Option<Node>)>>, Chan<Option<(int, Option<Node>)>>) = stream();
+    let (res_port, res_chan) = stream();
+    let body_chan = SharedChan::new(chan);
+    let body_port = SharedPort::new(port);
+
+    let loop_body: &fn() -> ~fn(int, uint) -> uint = || {
+        let (arc_port, arc_chan) = stream();
+        arc_chan.send(str_arc.clone());
+        let local_body_chan = body_chan.clone();
+
+        |idx:int, token_start:uint| {
+            // exclusive bound
+            let upper = num::min((idx as uint + 1) * iter_size, css_len);
+            let mut tokenizer = Tokenizer::new(arc_port.recv());
+            tokenizer.position = token_start;
+
+            // Reset the vector for this loop iteration
+            local_body_chan.send(Some((idx, None)));
+            while tokenizer.position < upper {
+                match tokenizer.next() {
+                    Some(node) => local_body_chan.send(Some((idx, Some(node)))),
+                    None => break
+                }
+            }
+            tokenizer.position
+        }
+    };
+
+    let predictor: &fn() -> ~fn(int) -> uint = || {
+        let (arc_port, arc_chan) = stream();
+        arc_chan.send(str_arc.clone());
+        |idx| {
+            next_token_start(arc_port.recv(), idx as uint * iter_size)
+        }
+    };
+
+    spawn_result_collector(body_port.clone(), res_chan, num_iters);
+    specfold(0, num_iters as int, loop_body, predictor);
+    body_chan.send(None);
+    res_port.recv()
 }
--- src/spec_css/test.rs
+++ src/spec_css/test.rs
@@ -1,130 +1,63 @@
 extern mod spec_css;
 extern mod css_lex;
 extern mod speculate;
+extern mod extra;
 
 use spec_css::*;
 use css_lex::*;
-use speculate::*;
-use std::{num, task, vec};
-use std::comm::{SharedPort, SharedChan, stream};
+use extra::arc::Arc;
+use extra::json;
+use extra::json::ToJson;
 
-/**
- * Collects the loop body results into a 2d vector.
- *
- * Each loop body sends its index and an optional result type. If the message
- * received over the `port` is `None`, then stop listening and push the result
- * over `chan`. If the message received is `Some(i, None)`, then clear the
- * `i`-th result vector. If the message is `Some(i, Some(t))`, then add `t` to
- * the `i`-th result vector.
- */
-fn spawn_result_collector<T: Send + Clone>(port: SharedPort<Option<(int, Option<T>)>>, chan: Chan<~[T]>, size: uint) {
-    do task::spawn {
-        let mut results = vec::from_elem::<~[T]>(size, Default::default());
-        loop {
-            match port.recv() {
-                Some((idx, Some(val))) => results[idx].push(val),
-                Some((idx, None)) => results[idx].clear(),
-                None => break
-            }
-        }
-        chan.send(results.flat_map(|v| v.clone()));
+fn run_json_tests<T: ToJson>(json_data: &str, parse: &fn (input: ~str) -> T) {
+    let items = match json::from_str(json_data) {
+        Ok(json::List(items)) => items,
+        _ => fail!("Invalid JSON")
+    };
+    assert!(items.len() % 2 == 0);
+    let mut input: Option<~str> = None;
+    for item in items.move_iter() {
+        match (&input, item) {
+            (&None, json::String(string)) => input = Some(string),
+            (&Some(_), expected) => {
+                let css = input.take_unwrap();
+                let result = parse(css.to_owned()).to_json();
+                if !json_almost_equals(&result, &expected) {
+                    fail!(format!("got: {}\nexpected: {}",
+                                  result.to_str(),
+                                  expected.to_str()));
+                }
+            },
+            _ => fail!("Unexpected JSON")
+        };
     }
 }
 
 // This could be replaced with JSON tests.
 #[test]
 fn test_next_token_start() {
-    let css = "cls1 : cls2 {prop: val;}";
+    let css = Arc::new(~"cls1 : cls2 {prop: val;}");
 
-    assert!(next_token_start(css, 8) == 11);
-    assert!(next_token_start(css, 4) == 4);
-    assert!(next_token_start(css, 13) == 13);
-    assert!(next_token_start(css, 14) == 17);
-    assert!(next_token_start(css, 0) == 0);
+    assert!(next_token_start(css.clone(), 8) == 11);
+    assert!(next_token_start(css.clone(), 4) == 4);
+    assert!(next_token_start(css.clone(), 13) == 13);
+    assert!(next_token_start(css.clone(), 14) == 17);
+    assert!(next_token_start(css.clone(), 0) == 0);
 }
 
 #[test]
 fn test_specfold_correct_prediction() {
-    let css = "cls1 : cls2 {prop: val;}";
-    let css_len = css.len();
-    let iter_size: uint = 8;
-    let num_iters: uint = 3;
-    let (port, chan): (Port<Option<(int, Option<Token>)>>, Chan<Option<(int, Option<Token>)>>) = stream();
-    let (res_port, res_chan) = stream();
-    let shared_chan = SharedChan::new(chan);
-    let shared_port = SharedPort::new(port);
-
-    let loop_body: &fn() -> ~fn(int, uint) -> uint = || {
-        let clone_chan = shared_chan.clone();
-        |idx:int, token_start:uint| {
-            // exclusive bound
-            let upper = num::min((idx as uint + 1) * iter_size, css_len);
-            let mut tokenizer = tokenize(css);
-            tokenizer.position = token_start;
-
-            // Reset the vector for this loop iteration
-            clone_chan.send(Some((idx, None)));
-            while tokenizer.position < upper {
-                match tokenizer.next() {
-                    Some((t, _)) => clone_chan.send(Some((idx, Some(t)))),
-                    None => break
-                }
-            }
-            tokenizer.position
-        }
-    };
-
-    let predictor: &fn() -> ~fn(int) -> uint = || {
-        |idx| next_token_start(css, idx as uint * iter_size)
-    };
-    spawn_result_collector(shared_port.clone(), res_chan, num_iters);
-    specfold(0, num_iters as int, loop_body, predictor);
-    shared_chan.send(None);
-    assert!(res_port.recv() == ~[Ident(~"cls1"), WhiteSpace, Colon, WhiteSpace,
-                                 Ident(~"cls2"), WhiteSpace, LeftCurlyBracket,
-                                 Ident(~"prop"), Colon, WhiteSpace,
-                                 Ident(~"val"), Semicolon, RightCurlyBracket])
+    let css = ~"cls1 : cls2 {prop: val;}";
+    assert!(spec_tokenize(css, 3).map(|n| match *n { (ref t, _) => t.clone() })
+            == ~[Ident(~"cls1"), WhiteSpace, Colon, WhiteSpace,
+                 Ident(~"cls2"), WhiteSpace, LeftCurlyBracket,
+                 Ident(~"prop"), Colon, WhiteSpace,
+                 Ident(~"val"), Semicolon, RightCurlyBracket])
 }
 
 #[test]
-fn test_specfold_incorrect_prediction() {
-    let css = "cls1 : cls2 {prop: val;}";
-    let css_len = css.len();
-    let iter_size: uint = 8;
-    let num_iters: uint = 3;
-    let (port, chan): (Port<Option<(int, Option<Token>)>>, Chan<Option<(int, Option<Token>)>>) = stream();
-    let (res_port, res_chan) = stream();
-    let shared_chan = SharedChan::new(chan);
-    let shared_port = SharedPort::new(port);
-
-    let loop_body: &fn() -> ~fn(int, uint) -> uint = || {
-        let clone_chan = shared_chan.clone();
-        |idx:int, token_start:uint| {
-            // exclusive bound
-            let upper = num::min((idx as uint + 1) * iter_size, css_len);
-            let mut tokenizer = tokenize(css);
-            tokenizer.position = token_start;
-
-            // Reset the vector for this loop iteration
-            clone_chan.send(Some((idx, None)));
-            while tokenizer.position < upper {
-                match tokenizer.next() {
-                    Some((t, _)) => clone_chan.send(Some((idx, Some(t)))),
-                    None => break
-                }
-            }
-            tokenizer.position
-        }
-    };
-
-    let predictor: &fn() -> ~fn(int) -> uint = || {
-        |_| 0
-    };
-    spawn_result_collector(shared_port.clone(), res_chan, num_iters);
-    specfold(0, num_iters as int, loop_body, predictor);
-    shared_chan.send(None);
-    assert!(res_port.recv() == ~[Ident(~"cls1"), WhiteSpace, Colon, WhiteSpace,
-                                 Ident(~"cls2"), WhiteSpace, LeftCurlyBracket,
-                                 Ident(~"prop"), Colon, WhiteSpace,
-                                 Ident(~"val"), Semicolon, RightCurlyBracket])
+fn test_spec_token_json() {
+    do run_json_tests(include_str!("../css_lex/css-lexing-tests/tokens.json")) |input| {
+        list_to_json(&spec_tokenize(input, 2))
+    }
 }
